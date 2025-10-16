951cdbbd09e3c4c3e8509984abc11cf4c535308e
Tanuja753 <tanujaargade5@gmail.com>
2025-10-16 14:46:22 +0530

    Initial commit

---
+++ assignmentno1.py
@@ -0,0 +1,93 @@
+#Q1
+print("hello core2web")  #hello core2web
+#Q2
+x=10
+print(x)        #10
+#Q3
+y=5.5
+print(type(y))  #<class 'float'>
+#Q4
+z=2+3j
+print(z)    #2+3j
+#Q5
+name="Tanuja"
+print(name) #Tanuja
+#Q6
+bool=True
+print(bool) #True
+#Q7
+a=25
+print(type(x))  #<class 'int'>
+#Q8
+b=31.5
+print(type(b))  #<class 'float'>
+#Q9
+num1=10
+num2= 20
+num3=20.5
+sum=num1+num2+num3
+print(sum)  #50.5
+#Q10
+list1=[10,20,"tanuja","neha",20.5]
+print(list1[0]) #10
+print(list1[1]) #20
+print(list1[2]) #tanuja
+print(list1[3]) #neha
+print(list1[4]) #20.5
+#Q11
+tupdata=(10,30,"tanuja","ravina",90)
+#Q12
+list2=[10,20,30,40]
+sum1=list2[0]+list2[1]+list2[2]+list2[3]
+print(sum1) #100
+#Q13
+set={10,20,30,40,50,10}
+print(set)#{50, 20, 40, 10, 30}
+#Q14
+list3=["tanuja","neha","ravina","payal","diya"]
+print(list3)#['tanuja', 'neha', 'ravina', 'payal', 'diya']
+list3[3]="core2web"
+print(list3)#['tanuja', 'neha', 'ravina', 'core2web', 'diya']
+#Q15
+dict1={"name":"ram","age":15,"marks":88}
+print(dict1["name"]) #ram
+print(dict1["age"]) #15
+print(dict1["marks"])   #88
+#Q16
+dict2={"name1":"virat","name2":"rohit","name3":"hardik"}
+print(dict2)    #{'name1': 'virat', 'name2': 'rohit', 'name3': 'hardik'}
+dict2["name2"]="jadeja"
+print(dict2) #{'name1': 'virat', 'name2': 'jadeja', 'name3': 'hardik'}
+#Q17
+dict={0:10,1:20,2:25,3:30,4:40}
+key=int(input("enter a key to search : ")) #3
+if key in dict:
+    print(key)  #3
+else:
+    print("key not exist in dictionary")
+sum2=dict[2]+dict[3]
+print(sum2) #55
+#Q18
+v= None
+v="tanuja"
+print(v)    #tanuja
+#Q19
+str1='tanuja'
+str2="sanjay"
+str3='''tanuja
+         argade'''
+print(str1) #tanuja
+print(str2) #sanjay
+print(str3) #tanuja
+            #          argade
+#Q20
+intnum=12
+floatnum=20.5
+complexnum= 2+3j
+str4="tanuja"
+list5=[1,2,3]
+tuple1=(10,20,30)
+runs={18:500,1:200,7:700}
+players={"rohit","virat","msd"}
+islighton=True
+run=None
